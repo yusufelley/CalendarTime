1426c88e91ca188ce197e76c2fd0e1f308895d44
Siddharth <siddharthjain1220@gmail.com>
2023-12-04 02:22:55 -0500

    add components to main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import TaskEventContainer from "./components/create/TaskEventContainer";
+import LeftPanel from './components/main-page/LeftPanel.jsx';
+import Header from './components/main-page/Header.jsx';
+import ContentGrid from './components/main-page/ContentGrid.jsx';
 
 function App() {
   return (
-    <>
-      {/* <h1>CalendarTime</h1> */}
-      {/* <TaskEventContainer /> */}
-    </>
+    // <>
+    //   <h1>CalendarTime</h1>
+    //   <TaskEventContainer />
+    // </>
+    <div className="app">
+      <LeftPanel selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="main-content">
+        <Header />
+        <ContentGrid selectedPage={selectedPage} />
+      </div>
+    </div>
   );
 }
 
