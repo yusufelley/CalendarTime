0fff01ddafe7c6ee8db5a53c3108777d5cf1e16c
Siddharth <siddharthjain1220@gmail.com>
2023-12-04 02:20:19 -0500

    add to-do list component to left panel

---
+++ frontend/src/components/main-page/ToDoList.jsx
@@ -0,0 +1,13 @@
+// CalendarGrid.js
+import React from "react";
+
+const ToDoList = () => {
+  return (
+    <div className="to-do-list">
+      {/* Render time slots and days here */}
+      TO DO LIST
+    </div>
+  );
+};
+
+export default ToDoList;
