5383af200443452c69d06584128eb2ce72def42b
Aeyan Ashraf <aeyan@Aeyans-MacBook-Air.local>
2023-12-05 23:25:11 -0500

    Week Scrolling

--- frontend/src/components/main-page/CalendarGrid.jsx
+++ frontend/src/components/main-page/CalendarGrid.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GoogleCalendarGrid.css"; // Import the CSS file
 
 const WeeklyCalendarGrid = () => {
@@ -11,6 +11,7 @@ const WeeklyCalendarGrid = () => {
     "Friday",
     "Saturday",
   ];
+
   const hoursOfDay = Array.from({ length: 24 * 4 }, (_, index) => index);
 
   // Example event data
@@ -20,52 +21,78 @@ const WeeklyCalendarGrid = () => {
     // Add more events as needed
   ];
 
-  // Function to check if an event exists at a specific day, hour, and quarter
   const hasEvent = (day, hour, quarter) =>
     events.some(
       (event) =>
         event.day === day && event.hour === hour && event.quarter === quarter
     );
 
+  const [currentWeek, setCurrentWeek] = useState(1);
+
+  const handleWeekChange = (newWeek) => {
+    setCurrentWeek(newWeek);
+  };
+
   return (
     <div className="weekly-calendar-container">
-      <table>
-        <thead>
-          <tr>
-            <th style={{ width: "10%" }}></th>
-            {daysOfWeek.map((day, dayIndex) => (
-              <th
-                key={dayIndex}
-                style={{ width: `${90 / daysOfWeek.length}%` }}
-              >
-                {day}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {hoursOfDay.map((timeSlot, timeIndex) => {
-            const hour = Math.floor(timeSlot / 4);
-            const quarter = (timeSlot % 4) * 15;
+      <div className="week-navigation">
+        <button
+          onClick={() => handleWeekChange(currentWeek - 1)}
+          disabled={currentWeek === 1}
+        >
+          {"<"}
+        </button>
+        <span>Week {currentWeek}</span>
+        <button
+          onClick={() => handleWeekChange(currentWeek + 1)}
+          disabled={currentWeek === 52}
+        >
+          {">"}
+        </button>
+      </div>
+      <div className="scrollable-calendar">
+        <div className="scrollable-weeks">
+          <div className="weeks-wrapper">
+            <table>
+              <thead>
+                <tr>
+                  <th style={{ width: "10%" }}></th>
+                  {daysOfWeek.map((day, dayIndex) => (
+                    <th
+                      key={dayIndex}
+                      style={{ width: `${90 / daysOfWeek.length}%` }}
+                    >
+                      {day}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {hoursOfDay.map((timeSlot, timeIndex) => {
+                  const hour = Math.floor(timeSlot / 4);
+                  const quarter = (timeSlot % 4) * 15;
 
-            return (
-              <tr key={timeIndex}>
-                <td className="hour-label">
-                  {`${hour}:${quarter === 0 ? "00" : quarter}`}
-                </td>
-                {daysOfWeek.map((day, dayIndex) => (
-                  <td
-                    key={dayIndex}
-                    className={`calendar-cell ${
-                      hasEvent(day, hour, timeSlot % 4) ? "has-event" : ""
-                    }`}
-                  ></td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  return (
+                    <tr key={timeIndex}>
+                      <td className="hour-label">
+                        {`${hour}:${quarter === 0 ? "00" : quarter}`}
+                      </td>
+                      {daysOfWeek.map((day, dayIndex) => (
+                        <td
+                          key={dayIndex}
+                          className={`calendar-cell ${
+                            hasEvent(day, hour, timeSlot % 4) ? "has-event" : ""
+                          }`}
+                        ></td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
