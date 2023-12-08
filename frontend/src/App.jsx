131e6b2ba375eb6171a116448441d45b15634686
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2023-12-08 15:14:16 -0500

    Updte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,45 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import CalendarEvent from "./components/CalendarEvent";
+import CalendarItem from "./components/CalendarItem";
 
 function App() {
   const [count, setCount] = useState(0);
+  const dummyTask = {
+    title: "Complete React Assignment",
+    deadline: new Date(2023, 11, 5), // December 5, 2023
+    description: "Finish the calendar component for the project.",
+    maxTimeBlock: 120, // 120 minutes
+    estimatedTimeToCompletion: 5, // 5 hours
+    startDate: new Date(2023, 11, 1), // December 1, 2023
+    color: "salmon"
+  };
 
+  const dummyEvent = {
+    title: "Team Meeting",
+    repeating: false,
+    when: {
+      date: new Date(2023, 11, 3), // December 3, 2023
+      time: "15:00" // 3:00 PM
+    },
+    location: "Conference Room B",
+    description: "Weekly sync-up with the team.",
+    color: "#ADD8E6" // Gold color
+  };
+  
   return (
-    <>
+    <div>
+      <h1>My Calendar</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Tasks</h2>
+        <CalendarItem item={dummyTask} itemType="task" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Events</h2>
+        <CalendarItem item={dummyEvent} itemType="event" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
---
+++ frontend/src/components/CalendarEvent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CalendarItem from './CalendarItem';
+
+const CalendarEvent = ({ event }) => {
+  return <CalendarItem item={event} itemType="event" />;
+};
+
+export default CalendarEvent;
