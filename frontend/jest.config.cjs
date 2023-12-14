c0c7fa0dd7b7eb368b13f31c4862ad4252683856
Aeyan Ashraf <aeyan@Aeyans-MacBook-Air.local>
2023-12-14 11:53:19 -0500

    testing #2

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,5 +1,12 @@
 // frontend/jest.config.js
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
+  coverageReporters: ["json", "lcov", "text", "clover"],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     "\\.(css|less)$": "/Users/aeyan/Documents/520/CalendarTime/frontend/tests/__mocks__/styleMock.js"
